196f6dd5aeefc7c40b4f0fc400dfe1e13687700a
hussain-shariff <hussainowaiz070@gmail.com>
2023-04-02 21:06:02 +0530

    monthly applications

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,9 +1,37 @@
 const jobsModel = require('../model/jobSchema')
 const authModel = require('../model/authSchema')
+const mongoose = require('mongoose')
+const moment = require('moment')
 
 const getAllJobs = async (req, res) =>{
     const jobs = await jobsModel.find({ createdBy : req.user.userID}).sort('createdAt')
-    res.json({ count : jobs.length, jobs})
+    let monthlyApplications = await jobsModel.aggregate([
+        { $match: { createdBy: new mongoose.Types.ObjectId(req.user.userID) } },
+        {
+            $group: {
+            _id: { year: { $year: '$createdAt' }, month: { $month: '$createdAt' } },
+            count: { $sum: 1 },
+            },
+        },
+        { $sort: { '_id.year': -1, '_id.month': -1 } },
+        { $limit: 6 },
+        ]);
+    monthlyApplications = monthlyApplications
+        .map((item) => {
+        const {
+            _id: { year, month },
+            count,
+        } = item;
+        const date = moment()
+            .month(month - 1)
+            .year(year)
+            .format('MMM Y');
+        return { date, count };
+    })
+    .reverse();
+    res.json({ count : jobs.length, jobs, monthlyApplications})
+
+    
 }
 const getJob = async (req, res) =>{
     const {id} = req.params
